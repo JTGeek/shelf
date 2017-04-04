47ba28f34bc794c96857cffde4c578c591033074
John French <hello@johnfrench.xyz>
2017-04-04 11:38:25 -0400

    Updating styles for series vol page

--- public/js/main.js
+++ public/js/main.js
@@ -21,4 +21,37 @@ $(document).ready(function () {
 	$('input').on('focus blur', function () {
 		$(this).parent().toggleClass('focused');
 	});
+
+	// Slide Toggle + Fade Function
+	$.fn.slideFadeToggle = function (speed, easing, callback) {
+		return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+	};
+
+	$('.want-have-select, .want-selected, .have-selected').hide();
+
+	// Want / Have Button
+	$('.want-have-default').on('click', function () {
+		$('.want-have-default').hide();
+		$('.want-have-select').show();
+	});
+	// Want / Have Select (Want)
+	$('.want-have-select .want').on('click', function () {
+		$('.want-have-select').hide();
+		$('.want-selected').show();
+	});
+	// Want / Have Select (Have)
+	$('.want-have-select .have').on('click', function () {
+		$('.want-have-select').hide();
+		$('.have-selected').show();
+	});
+	// Want / Have Select (Close)
+	$('.want-have-select .close').on('click', function () {
+		$('.want-have-select').hide();
+		$('.want-have-default').show();
+	});
+	// Deselect
+	$('.want-selected, .have-selected').on('click', function () {
+		$('.want-selected, .have-selected').hide();
+		$('.want-have-select').show();
+	});
 });
